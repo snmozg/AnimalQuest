a34f3b05073dcd57e6de574e28e209da7b900388
sozge <sozgeozkartal@gmail.com>
2025-01-23 22:47:53 +0300

    components created

--- app/src/main/java/com/sozge/animalquest/Components.kt
+++ app/src/main/java/com/sozge/animalquest/Components.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -30,3 +31,18 @@ import androidx.compose.ui.unit.sp
              contentDescription = "app logo")
      }
  }
+
+@Composable
+fun TextComponent(
+    textValue: String,
+    textSize: TextUnit,
+    colorValue : Color = Color.Black,
+    ) {
+    Text(
+        text = textValue,
+        color = colorValue,
+        fontSize = textSize
+    )
+
+
+}
--- app/src/main/java/com/sozge/animalquest/ui/screens/userInputScreen.kt
+++ app/src/main/java/com/sozge/animalquest/ui/screens/userInputScreen.kt
@@ -3,16 +3,20 @@ package com.sozge.animalquest.ui.screens
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.sozge.animalquest.TextComponent
 import com.sozge.animalquest.TopBar
 
 
@@ -31,6 +35,19 @@ fun userInputScreen(
 
         ) {
             TopBar("Hi There! \uD83D\uDE0B")
+            
+            TextComponent(
+                textValue = "Let's learn about You!" ,
+                textSize = 24.sp)
+
+            Spacer(modifier =Modifier.size(20.dp))
+            TextComponent(
+                textValue = "This app will prepare a details page based on input provided by you." ,
+                textSize = 18.sp)
+
+            Spacer(modifier =Modifier.size(60.dp))
+
         }
     }
 }
+
