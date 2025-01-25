b9b1850841805951fa7c1fb00908ed9c9a5f09e3
sozge <sozgeozkartal@gmail.com>
2025-01-25 20:24:35 +0300

    image transfer

--- app/src/main/java/com/sozge/animalquest/ui/screens/welcomeScreen.kt
+++ app/src/main/java/com/sozge/animalquest/ui/screens/welcomeScreen.kt
@@ -1,5 +1,6 @@
 package com.sozge.animalquest.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -22,12 +23,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.sozge.animalquest.R
 import com.sozge.animalquest.TextComponent
 import com.sozge.animalquest.TopBar
 import com.sozge.animalquest.ui.animalFacts
@@ -37,36 +41,67 @@ import com.sozge.animalquest.ui.userInputViewModel
 fun welcomeScreen(
     navController: NavController,
     selectedAnimal: String,
-    userInputViewModel: userInputViewModel,
+    userInputViewModel: userInputViewModel,) {
 
-    ) {
     val facts = animalFacts[selectedAnimal] ?: emptyList()
     val randomFact = facts.randomOrNull() ?: "No facts available."
 
-    Surface(
-        modifier = Modifier.fillMaxSize()
-    ) {
+
+    val animalImage = when (selectedAnimal) {
+            "Cat"->  R.drawable.cat
+            "Dog"->  R.drawable.dog
+            "Koala"->  R.drawable.koala
+            "Fish"->  R.drawable.fish
+            "Hamster"->  R.drawable.hamster
+            "Cow"-> R.drawable.cow
+            "Monkey"->  R.drawable.monkey
+            "Bear"-> R.drawable.love
+            "Horse"-> R.drawable.horse
+        else -> R.drawable.hamster
+    }
+
+            Surface(
+            modifier = Modifier.fillMaxSize()
+            ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(10.dp),
-            verticalArrangement = Arrangement.SpaceEvenly,
-            horizontalAlignment = Alignment.Start
+                .padding(20.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
-            TopBar("Did You Know? 🐾")
+            TopBar("Did You Know? ")
 
-            Spacer(modifier = Modifier.height(30.dp))
+            Spacer(modifier = Modifier.size(30.dp))
+
+            TextComponent(
+                textValue = "Here is the animal you chose and some information about it.",
+                textSize = 24.sp
+            )
+
+            Spacer(modifier = Modifier.size(30.dp))
 
 
+            Box(
+                modifier = Modifier
+                    .size(150.dp)
+                    .padding(16.dp),
+                    contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = animalImage),
+                    contentDescription = "$selectedAnimal Image",
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+            }
 
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp, vertical = 12.dp),
-
                 shape = RoundedCornerShape(16.dp),
-
             ) {
                 Column(
                     modifier = Modifier
@@ -83,7 +118,6 @@ fun welcomeScreen(
 
                     Spacer(modifier = Modifier.height(16.dp))
 
-
                     Box(
                         modifier = Modifier
                             .fillMaxWidth()
@@ -98,21 +132,24 @@ fun welcomeScreen(
                         Text(
                             text = randomFact,
                             style = TextStyle(fontSize = 20.sp, color = Color.Black),
-                            modifier = Modifier.fillMaxSize().padding(8.dp)
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(8.dp)
                         )
                     }
                 }
             }
 
             Spacer(modifier = Modifier.height(40.dp))
-            Button(
 
+            Button(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp),
-
-                onClick = { userInputViewModel.resetState()
-                        navController.navigateUp() },
+                onClick = {
+                    userInputViewModel.resetState()
+                    navController.navigateUp()
+                },
                 colors = ButtonDefaults.buttonColors(Color.LightGray)
             ) {
                 Text(
